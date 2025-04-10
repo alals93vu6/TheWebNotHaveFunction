21c24036d4cd938aca87f68812b5be406f2da1c4
alals93uv6 <a0987915131@gmail.com>
2025-04-10 15:39:01 +0800

    Level 4 Down

--- TheWebNotHaveFunction/JS/LV4/GameManager_lv4.js
+++ TheWebNotHaveFunction/JS/LV4/GameManager_lv4.js
@@ -35,6 +35,20 @@ export async function OnDestroyEvent()
     document.getElementsByClassName("winAlert")[0].style.opacity = "1";
     await delay(500);
     Animator.ReadyDestory();
+
+    await delay(12000);
+    ENDEvent();
+}
+
+async function ENDEvent() {
+    ReadyToENDText();
+    await delay(1000);
+    document.getElementById("theFire").style.display = "none";
+    document.getElementsByClassName("winAlert")[0].style.display = "none";
+    await delay(1200);
+    document.getElementById("W7").innerText = "功";
+    await delay(1500);
+    window.location.href = "Level_5.html";
 }
 
 export async function OnButtonA()
@@ -77,6 +91,16 @@ export function OnButtonE()
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function ReadyToENDText()
+{
+    document.getElementById("theOffScreen").innerText = "什麼鬼？！這裡又發生甚麼事了？？！！";
+    document.getElementById("W6").innerText = "有";
+    document.getElementById("W7").innerText = "龔";
+    document.getElementById("W8").innerText = "能";
+    await delay(2000);
+    document.getElementById("theOffScreen").innerText = "";
 }
 
 window.GameAPI = {
